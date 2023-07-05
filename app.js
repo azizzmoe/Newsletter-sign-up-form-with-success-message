48e9a4621aeb160abba96ebf19170b6853721f3b
azizzmoe <aziznageeye9@gmail.com>
2023-07-05 23:59:08 +0300

    update 06

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const form = document.querySelector('form')
-const main = document.querySelector("main")
+const container = document.querySelector(".container")
 const popup = document.querySelector(".popup")
 const dismiss = document.querySelector(".dismiss")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    main.style.display = "none"
+    container.style.display = "none"
     popup.style.display = "block"
 })
 
 dismiss.addEventListener("click", () => {
-    main.style.display = "block"
+    container.style.display = "block"
     popup.style.display = "none"
 })
