e1cbb354496beaf849c99d1557bf62312a6df8e7
azizzmoe <aziznageeye9@gmail.com>
2023-07-19 23:31:29 +0300

    update 08

--- app.js
+++ app.js
@@ -2,14 +2,33 @@ const form = document.querySelector('form')
 const container = document.querySelector(".container")
 const popup = document.querySelector(".popup")
 const dismiss = document.querySelector(".dismiss")
+const emailDisplay = document.querySelector(".email-display")
+const emailInput = document.querySelector('#email');
+const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const alert = document.querySelector(".invalid-email")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    container.style.display = "none"
-    popup.style.display = "block"
+    if (!emailRegex.test(emailInput.value)) {
+        // Show an error message or highlight the email input
+    } else {
+        container.style.display = "none"
+        popup.style.display = "block"
+        emailDisplay.textContent = emailInput.value;
+    }
 })
 
 dismiss.addEventListener("click", () => {
     container.style.display = "flex"
     popup.style.display = "none"
 })
+
+emailInput.addEventListener('input', () => {
+    if (!emailRegex.test(emailInput.value)) {
+        emailInput.classList.add('invalid-border', 'invalid-input');
+        alert.style.display = "flex"
+    } else {
+        emailInput.classList.remove('invalid-border', 'invalid-input');
+        alert.style.display = "none"
+    }
+});
